4d24cdd351acb315da19abf28186f5f38f473924
unknown <theo.canonne99@laposte.net>
2021-09-16 18:31:25 +0200

    update to hide code for 17/09 <- to remove

--- src/App.vue
+++ src/App.vue
@@ -63,11 +63,11 @@ const cards = ref([
   },
   {
     name: "Age",
-    values: ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+    values: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
     color: "#00BA34",
     isImage: false,
   },
-  {
+  /*{
     name: "Phone Number",
     values: [
       "731-8562",
@@ -111,7 +111,7 @@ const cards = ref([
     values: images.addresses,
     color: "#DE0093",
     isImage: true,
-  },
+  },*/
 ]);
 
 function resetFlipped() {
